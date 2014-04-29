7ee7d11eccc01d813bd9e79c24deaadae41bd707
christian-lang-neumont <clang@student.neumont.edu>
2014-04-28 20:21:42 -0600

    everything but the lerp function works

--- GameSkeleton/GameSkeleton/GameSolution/Engine/Vector2.h
+++ GameSkeleton/GameSkeleton/GameSolution/Engine/Vector2.h
@@ -1,64 +1,145 @@
+#ifndef VECTOR2_H
+#define VECTOR2_H
+
+
 #include<iostream>
 using std::cout;
 
-struct Vector2
-{
-	float X;
-	float Y;
-	Vector2(float x = 0, float y = 0): X(x), Y(y)
+namespace Engine{
+
+	struct Vector2
 	{
-		X = x;
-		Y = y;
+		float X;
+		float Y;
+		Vector2(float x = 0.0f, float y = 0.0f): X(x), Y(y)
+		{
+
+		}
+
+		operator float*() 
+		{
+			return &X;
+		}
+
+		inline Vector2 LERP(Vector2 a, Vector2 b, float beta)
+		{
+			float lerpX = ((1-beta) * a.X) + (beta * b.X);
+			float lerpY = ((1-beta) * a.Y) + (beta * b.Y);
+			Vector2 lerpVector(lerpX, lerpY);
+			return lerpVector;
+
+		}
+
+		inline float Length()
+		{
+			float xSquared = X * X;
+			float ySquared = Y * Y;
+			float underRadical = xSquared + ySquared;
+			float result = sqrt(underRadical);
+			return result;
+		}
+
+		inline float LengthSquared()
+		{
+			return sqrt(Length());
+		}
+
+		inline Vector2 Normalized()
+		{
+
+			float resX = X / Length();
+			float resY = Y / Length();
+			Vector2 result(resX, resY);
+			return result;
+		}
+
+		inline Vector2 PerpCW()
+		{
+			Vector2 cwVector(Y, (-1 * X));
+			return cwVector;
+		}
+
+		inline Vector2 PerpCCW()
+		{
+			Vector2 cwVector((-1 * Y), X);
+			return cwVector;
+		}
+
+		float Dot(const Vector2& left, const Vector2& right)
+		{
+			return (left.X * right.Y) + (left.Y * right.X);
+		}
+
+	};
+
+	static Vector2 LERP(Vector2 a, Vector2 b, float beta)
+	{
+		float lerpX = ((1-beta) * a.X) + (beta * b.X);
+		float lerpY = ((1-beta) * a.Y) + (beta * b.Y);
+		Vector2 lerpVector(lerpX, lerpY);
+		return lerpVector;
+
 	}
-};
 
-Vector2 operator+(const Vector2& left, const Vector2& right)
-{
-	float resX = left.X + right.X;
-	float resY = left.Y + right.Y;
-	Vector2 result(resX, resY);
-	return result;
+	static float Dot(const Vector2& left, const Vector2& right)
+	{
+		return (left.X * right.Y) + (left.Y * right.X);
+	}
+
+	Vector2 operator+(const Vector2& left, const Vector2& right)
+	{
+		float resX = left.X + right.X;
+		float resY = left.Y + right.Y;
+		Vector2 result(resX, resY);
+		return result;
+	}
+
+	Vector2 operator-(const Vector2& left, const Vector2& right)
+	{
+		float resX = left.X + (-1 * right.X);
+		float resY = left.Y + (-1 * right.Y);
+		Vector2 result(resX, resY);
+		return result;
+	}
+
+	std::ostream& operator<<(std::ostream& stream, const Vector2& right)
+	{
+		cout << "{x:" << right.X << ", y:" << right.Y << "}";
+		return stream;
+	}
+
+	Vector2 operator*(const float scalar, const Vector2& right)
+	{
+		float resX = (scalar * right.X);
+		float resY = (scalar * right.Y);
+		Vector2 result(resX, resY);
+		return result;
+	}
+
+	Vector2 operator*(const Vector2& left, const float scalar)
+	{
+		float resX = (scalar * left.X);
+		float resY = (scalar * left.Y);
+		Vector2 result(resX, resY);
+		return result;
+	}
+
+	Vector2 operator/(const float scalar, const Vector2& right)
+	{
+		float resX = (scalar / right.X);
+		float resY = (scalar / right.Y);
+		Vector2 result(resX, resY);
+		return result;
+	}
+
+	Vector2 operator/(const Vector2& right, const float scalar)
+	{
+		float resX = (right.X / scalar);
+		float resY = (right.Y / scalar);
+		Vector2 result(resX, resY);
+		return result;
+	}
+
 }
 
-Vector2 operator-(const Vector2& left, const Vector2& right)
-{
-	float resX = left.X + (-1 * right.X);
-	float resY = left.Y + (-1 * right.Y);
-	Vector2 result(resX, resY);
-	return result;
-}
-
-std::ostream& operator<<(std::ostream& stream, const Vector2& right)
-{
-	cout << "{x:" << right.X << ", y:" << right.Y << "}";
-	return stream;
-}
-
-Vector2 operator*(const float scalar, const Vector2& right)
-{
-	float resX = (scalar * right.X);
-	float resY = (scalar * right.Y);
-	Vector2 result(resX, resY);
-	return result;
-}
-
-Vector2 operator*(const Vector2& left, const float scalar)
-{
-	float resX = (scalar * left.X);
-	float resY = (scalar * left.Y);
-	Vector2 result(resX, resY);
-	return result;
-}
-
-Vector2 operator/(const float scalar, const Vector2& right)
-{
-	float resX = (scalar / right.X);
-	float resY = (scalar / right.Y);
-	Vector2 result(resX, resY);
-	return result;
-}
-
-Vector2 operatorLERP(const Vector2& left, const Vector2& right, const int beta)
-{
-
-}
+#endif
--- GameSkeleton/GameSkeleton/TwoDCalculator/EntryPoint.cpp
+++ GameSkeleton/GameSkeleton/TwoDCalculator/EntryPoint.cpp
@@ -1,11 +1,84 @@
 #include "RenderUI.h"
 #include "Engine.h"
+#include "Vector2.h"
+using Engine::Vector2;
+
+Vector2 leftVector;
+Vector2 rightVector;
+Vector2 resultVector;
+void myBasicVectorEquationData(const BasicVectorEquationInfo& data)
+{
+	leftVector = data.scalar1 * Vector2(data.x1, data.y1);
+	rightVector = data.scalar2 * Vector2(data.x2, data.y2);
+
+	resultVector = data.add ? leftVector + rightVector : leftVector - rightVector;
+}
+
+Vector2 originalVector;
+Vector2 normalVector;
+Vector2 cwPerpindicularVector;
+Vector2 ccwPerpendicularVector;
+void myPerpendicularDataCallback(const PerpendicularData& data)
+{
+	originalVector = Vector2(data.x, data.y);
+	normalVector = originalVector.Normalized();
+	cwPerpindicularVector = originalVector.PerpCW();
+	ccwPerpendicularVector = originalVector.PerpCCW();
+}
+
+Vector2 dotProductVector1;
+Vector2 dotProductVector2;
+Vector2 dotProductProjectionVector;
+Vector2 dotProductRejectionVector;
+void myDotProductDataCallback(const DotProductData& data)
+{
+	dotProductVector1 = Vector2(data.v1i, data.v1j);
+	dotProductVector2 = Vector2(data.v2i, data.v2j);
+
+	if(data.projectOntoLeftVector)
+	{
+		dotProductProjectionVector = dotProductVector1.Normalized() * Engine::Dot(dotProductVector2, dotProductVector1.Normalized());
+		dotProductRejectionVector = dotProductVector2 - dotProductProjectionVector;
+	}
+	else
+	{
+		dotProductProjectionVector = dotProductVector2.Normalized() * Engine::Dot(dotProductVector1, dotProductVector2.Normalized());
+		dotProductRejectionVector = dotProductVector1 - dotProductProjectionVector;
+	}
+}
+
+Vector2 lerpAVector;
+Vector2 lerpBVector;
+Vector2 lerpAMinusBVector;
+Vector2 lerpAVectorLerpPortion;
+Vector2 lerpBVectorLerpPortion;
+Vector2 lerpLerpResultVector;
+void myLerpDataCallback(const LerpData& data)
+{
+	lerpAVector = Vector2(data.a_i, data.a_j);
+	lerpBVector = Vector2(data.b_i, data.b_j);
+
+	lerpAMinusBVector = lerpBVector - lerpAVector;
+	
+	lerpAVectorLerpPortion = Engine::LERP(lerpAVector, lerpBVector, data.beta);
+	lerpBVectorLerpPortion = Engine::LERP(lerpBVector, lerpAVector, data.beta);
+
+	lerpLerpResultVector = lerpAVectorLerpPortion + lerpBVectorLerpPortion;
+
+}
+
 
 int main(int argc, char* argv[])
 {
 	Engine::Init();
 
 	RenderUI renderUI;
+
+
+	renderUI.setBasicVectorEquationData(myBasicVectorEquationData, &leftVector.X, &rightVector.X, &resultVector.X);
+	renderUI.setPerpendicularData(&originalVector.X, &normalVector.X, &cwPerpindicularVector.X, &ccwPerpendicularVector.X, myPerpendicularDataCallback);
+	renderUI.setDotProductData(&dotProductVector1.X, &dotProductVector2.X, &dotProductProjectionVector.X, &dotProductRejectionVector.X, myDotProductDataCallback);
+	renderUI.setLerpData(&lerpAVector.X, &lerpBVector.X, &lerpAMinusBVector.X, &lerpAVectorLerpPortion.X, &lerpBVectorLerpPortion.X, &lerpLerpResultVector.X, myLerpDataCallback); 
 
 	if( ! renderUI.initialize(argc, argv))
 		return -1;
